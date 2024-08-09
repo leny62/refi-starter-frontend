a6fd910db3d4d07ccea7aa77828248aee5810f47
leny62 <lihirwe6@gmail.com>
2024-08-09 13:50:01 +0200

    add accessible section

--- src/pages/home/Home_01.jsx
+++ src/pages/home/Home_01.jsx
@@ -1,6 +1,6 @@
 import Hero from '../../components/sections/home_01/Hero';
 import Service from '../../components/sections/home_01/service/Service';
-// import Content_01 from '../../components/sections/home_01/Content_01';
+import Content_01 from '../../components/sections/home_01/Content_01';
 // import Content_02 from '../../components/sections/home_01/Content_02';
 // import FunFact from '../../components/sections/home_01/FunFact';
 // import Pricing from '../../components/sections/home_01/Pricing';
@@ -11,8 +11,8 @@ const Home_01 = () => {
     <main className='main-wrapper relative overflow-hidden'>
       <Hero />
       <Service />
-      {/* <Content_01 />
-      <Content_02 />
+      <Content_01 />
+      {/* <Content_02 />
       <FunFact />
       <Pricing />
       <Testimonial_Section /> */}
